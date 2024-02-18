9c103d50710c54b24fc6f17fc1b078584e4a5405
rohit8450 <rohitrajthakur10@gmail.com>
2024-02-18 22:15:23 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,11 +2,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const db =  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB Atlas");
-});
+const db =  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("CONNECTION ESTABLISHED"));
 module.exports = db;
 
